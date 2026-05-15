c268f75f31357a369fa822553b362cc46f6e46bf
Tuyeenf <phamvantuyen1110@gmail.com>
2026-05-15 18:01:32 +0700

    Initial commit - Gán dự án vào repo

---
+++ app/src/main/java/com/example/flickfind_ltttbdd/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.flickfind_ltttbdd
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.flickfind_ltttbdd.ui.theme.FlickFindLTTTBDDTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FlickFindLTTTBDDTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FlickFindLTTTBDDTheme {
+        Greeting("Android")
+    }
+}
