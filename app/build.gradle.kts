a9fc3122475ebf5bc4e37703dead2a3258a99c98
Tuyeenf <phamvantuyen1110@gmail.com>
2026-05-17 02:30:20 +0700

    Cấu hình: Cài đặt các Dependency nền tảng cho dự án FlickFind

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
+
 }
 
 android {
@@ -40,6 +42,24 @@ android {
 }
 
 dependencies {
+    // 1. Giao diện & Định hướng (UI & Navigation)
+    implementation("androidx.navigation:navigation-compose:2.8.5")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
+
+    // 2. Mạng (Retrofit để gọi MockAPI)
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+
+    // 3. Cơ sở dữ liệu nội bộ (Room DB)
+    val roomVersion = "2.6.1"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    // Dòng dưới này dùng để xử lý sinh code tự động cho Room
+    ksp("androidx.room:room-compiler:$roomVersion")
+
+    // 4. Hiển thị ảnh từ URL (Coil)
+    implementation("io.coil-kt:coil-compose:2.6.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
