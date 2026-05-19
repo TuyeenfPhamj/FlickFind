6fa26f42da6fa9d5a1ebd871fcae36b41eee1da7
Tuyeenf <phamvantuyen1110@gmail.com>
2026-05-19 13:24:42 +0700

    Sửa lại ảnh logo, thêm phiên bản logo phụ và tạm thời comment NavGraph để tránh lỗi

--- app/src/main/java/com/example/flickfind_ltttbdd/navigation/NavGraph.kt
+++ app/src/main/java/com/example/flickfind_ltttbdd/navigation/NavGraph.kt
@@ -18,9 +18,9 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.example.flickfind_ltttbdd.ui.viewmodel.AppViewModelProvider
-import com.example.flickfind_ltttbdd.ui.viewmodel.HomeViewModel
-import com.example.flickfind_ltttbdd.ui.screens.HomeScreen
+//import com.example.flickfind_ltttbdd.ui.viewmodel.AppViewModelProvider
+//import com.example.flickfind_ltttbdd.ui.viewmodel.HomeViewModel
+//import com.example.flickfind_ltttbdd.ui.screens.HomeScreen
 
 @Composable
 fun MainNavGraph() {
@@ -37,13 +37,13 @@ fun MainNavGraph() {
             modifier = Modifier.padding(innerPadding)
         ) {
             // Màn hình 1: Khám phá (Trang chủ của bạn)
-            composable(Screen.Home.route) {
-                val context = LocalContext.current
-                val homeViewModel: HomeViewModel = viewModel(
-                    factory = AppViewModelProvider(context)
-                )
-                HomeScreen(viewModel = homeViewModel, navController = navController)
-            }
+//            composable(Screen.Home.route) {
+//                val context = LocalContext.current
+//                val homeViewModel: HomeViewModel = viewModel(
+//                    factory = AppViewModelProvider(context)
+//                )
+//                HomeScreen(viewModel = homeViewModel, navController = navController)
+//            }
 
             // Màn hình 2: Cá nhân (Giao diện phụ trách của thành viên khác)
             composable(Screen.Profile.route) {
