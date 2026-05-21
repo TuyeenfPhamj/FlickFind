e11db973d8958d9bd2e589cda5c7cc0f704af2e8
Tuyeenf <phamvantuyen1110@gmail.com>
2026-05-21 22:17:00 +0700

    Sửa lại DATA: Lấy danh sách phim yêu thích theo từng id tài khoản

--- app/src/main/java/com/example/flickfind_ltttbdd/data/MovieRepository.kt
+++ app/src/main/java/com/example/flickfind_ltttbdd/data/MovieRepository.kt
@@ -18,10 +18,26 @@ class MovieRepository(
     // 1. PHẦN XỬ LÝ MOCK API (REMOTE)
     // ==========================================
 
-    // Hàm lấy danh sách phim phân trang từ API, bọc trong Result để bắt lỗi mạng (nếu có)
-    suspend fun getMoviesFromApi(page: Int, limit: Int): Result<List<MovieResponse>> {
+    // Hàm lấy danh sách phim phân trang từ API có kèm bộ lọc, bọc trong Result để bắt lỗi mạng
+    suspend fun getMoviesFromApi(
+        page: Int,
+        limit: Int,
+        search: String? = null,
+        genre: String? = null,
+        yearRange: String? = null
+    ): Result<List<MovieResponse>> {
         return try {
-            val response = apiService.getMovies(page, limit)
+            val response = apiService.getMovies(page, limit, search, genre, yearRange)
+            Result.success(response)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    // Lấy chi tiết một bộ phim từ API theo ID
+    suspend fun getMovieByIdFromApi(movieId: Int): Result<MovieResponse> {
+        return try {
+            val response = apiService.getMovieById(movieId)
             Result.success(response)
         } catch (e: Exception) {
             Result.failure(e)
@@ -34,7 +50,7 @@ class MovieRepository(
     // ==========================================
 
     // Lấy luồng dữ liệu danh sách phim yêu thích thời gian thực
-    fun getAllFavorites(): Flow<List<FavoriteMovieEntity>> = movieDao.getAllFavorites()
+    fun getAllFavorites(userId: String): Flow<List<FavoriteMovieEntity>> = movieDao.getAllFavorites(userId)
 
     // Thêm một phim vào danh sách yêu thích
     suspend fun addToFavorite(movie: FavoriteMovieEntity) {
@@ -47,8 +63,8 @@ class MovieRepository(
     }
 
     // Kiểm tra xem phim này đã được lưu trong Room chưa
-    suspend fun isMovieFavorite(movieId: Int): Boolean {
-        return movieDao.getMovieById(movieId) != null
+    suspend fun isMovieFavorite(movieId: Int, userId: String): Boolean {
+        return movieDao.getMovieById(movieId, userId) != null
     }
 
 
@@ -57,7 +73,12 @@ class MovieRepository(
     // ==========================================
 
     // Lấy thông tin User profile
-    fun getUserProfile(): Flow<UserEntity?> = userDao.getUserProfile()
+    fun getUserProfile(userId: String): Flow<UserEntity?> = userDao.getUserProfile(userId)
+
+    // Cập nhật hoặc khởi tạo User
+    suspend fun insertOrUpdateUser(user: UserEntity) {
+        userDao.insertOrUpdateUser(user)
+    }
 
     // Cập nhật thông tin User (CRUD - Update)
     suspend fun updateUserProfile(user: UserEntity) {
