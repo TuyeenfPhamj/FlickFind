ec44fde4cd367456e2348bcd2b10864479f124f2
Tuyeenf <phamvantuyen1110@gmail.com>
2026-05-22 22:24:51 +0700

    Cấu hình: màu cho dark / light theme,màu thanh nav theo theme, thêm logo trong app cho light theme

--- app/src/main/java/com/example/flickfind_ltttbdd/navigation/NavGraph.kt
+++ app/src/main/java/com/example/flickfind_ltttbdd/navigation/NavGraph.kt
@@ -65,13 +65,10 @@ fun AppBottomNavigationBar(navController: NavHostController) {
         Screen.Profile,
         Screen.About
     )
-    val AppSurface = Color(0xFF171E30)    // Màu thanh điều hướng (trùng màu ô tìm kiếm của bạn)
-    val AppIndicator = Color(0xFF1A2844)  // Màu vòng bo (viên nhộng) bọc Icon khi được chọn
-    val TextActive = Color(0xFFFFFFFF)    // Chữ và Icon sáng trắng khi Active
-    val TextInactive = Color(0xFF8E9AA6)
+
     NavigationBar(
-        containerColor = AppSurface,
-        tonalElevation = 0.dp
+        containerColor = MaterialTheme.colorScheme.surface,
+        tonalElevation = 8.dp
     ) {
         // Lấy trạng thái màn hình hiện tại để check làm sáng nút
         val navBackStackEntry by navController.currentBackStackEntryAsState()
@@ -97,19 +94,20 @@ fun AppBottomNavigationBar(navController: NavHostController) {
                 label = {
                     Text(
                         text = screen.title,
-                        color = if (isSelected) TextActive else TextInactive
+                        color = if (isSelected) MaterialTheme.colorScheme.primary 
+                                else MaterialTheme.colorScheme.onSurfaceVariant
                     )
                 },
                 icon = {
                     Icon(
-                        // Sửa lỗi ImageVector? bằng cách thêm toán tử dự phòng ?:
                         imageVector = screen.icon ?: Icons.Default.Home,
                         contentDescription = screen.title,
-                        tint = if (isSelected) TextActive else TextInactive
+                        tint = if (isSelected) MaterialTheme.colorScheme.primary 
+                               else MaterialTheme.colorScheme.onSurfaceVariant
                     )
                 },
                 colors = NavigationBarItemDefaults.colors(
-                    indicatorColor = AppIndicator
+                    indicatorColor = MaterialTheme.colorScheme.secondaryContainer
                 )
             )
         }
